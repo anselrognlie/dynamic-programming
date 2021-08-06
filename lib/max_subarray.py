aa1953880e3f99d63dbb9d56ab9293c000d93f0a
Chris McAnally <chris@adadevelopersacademy.org>
2021-08-06 12:49:26 -0700

    starter code for lib folder

--- lib/max_subarray.py
+++ lib/max_subarray.py
@@ -1,8 +1,12 @@
 
-# Time Complexity: ?
-# Space Complexity: ?
-def max_sub_array(nums)
-    return 0 if nums == nil
-    
-    raise NotImplementedError, "Method not implemented yet!"
-end
+def max_sub_array(nums):
+    """ Returns the max subarray of the given list of numbers.
+        Returns 0 if  nums is None or an empty list.
+        Time Complexity: ?
+        Space Complexity: ?
+    """
+    if nums == None:
+        return 0
+    if len(nums) == 0:
+        return 0
+    pass
--- lib/newman_conway.py
+++ lib/newman_conway.py
@@ -2,6 +2,9 @@
 
 # Time complexity: ?
 # Space Complexity: ?
-def newman_conway(num)
-  raise NotImplementedError, "newman_conway isn't implemented"
-end
+def newman_conway(num):
+    """ Returns the Newman Conway number for the given num.
+        Time Complexity: ?
+        Space Complexity: ?
+    """
+    pass
