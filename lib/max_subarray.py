40ffbef1f12b69f4fae84bee81eb8ad4e64a961c
Ansel Rognlie <36503186+anselrognlie@users.noreply.github.com>
2022-07-12 13:07:23 -0700

    add implementation

--- lib/max_subarray.py
+++ lib/max_subarray.py
@@ -9,4 +9,17 @@ def max_sub_array(nums):
         return 0
     if len(nums) == 0:
         return 0
-    pass
+
+    max_so_far = nums[0]
+    max_ending_here = 0
+
+    for num in nums:
+        max_ending_here += num
+
+        if max_ending_here > max_so_far:
+            max_so_far = max_ending_here
+
+        if max_ending_here < 0:
+            max_ending_here = 0
+
+    return max_so_far
--- lib/newman_conway.py
+++ lib/newman_conway.py
@@ -4,7 +4,23 @@
 # Space Complexity: ?
 def newman_conway(num):
     """ Returns a list of the Newman Conway numbers for the given value.
-        Time Complexity: ?
-        Space Complexity: ?
+        Time Complexity: O(n) where n is the number of terms
+        Space Complexity: O(n) where n is the number of terms
+                          a little wiggle since stringifying the ints will get
+                          longer the larger the terms
     """
-    pass
+    if num < 1:
+        raise ValueError
+
+    if num == 1:
+        nums = [1]
+    else:        
+        nums = [1] * num
+
+    for i in range(3, num + 1):
+        p_nm1 = nums[i - 1 - 1]
+        p_p_nm1 = nums[p_nm1 - 1]
+        p_nmp_nm1 = nums[i - p_nm1 - 1]
+        nums[i - 1] = p_p_nm1 + p_nmp_nm1
+
+    return " ".join(map(str, nums))
